5325104dae9a9f2e92248e6f5a7ca024ed97ef6c
Rishabh Goyal <115866715+Rishabhgoyal0183@users.noreply.github.com>
2024-07-22 19:58:44 +0530

    Domax - Hosting Website
    Welcome to the Domax Hosting Website repository! This project is a comprehensive static website that provides detailed information about web hosting services. The website is built using HTML, CSS, and JavaScript to ensure a smooth and engaging user experience.
    
    Features
    Informative Content: Covers all aspects of web hosting, including types of hosting, benefits, and tips for choosing the right hosting service.
    Responsive Design: Ensures the website is accessible and looks great on all devices, from desktops to smartphones.
    Interactive Elements: Utilizes JavaScript to add interactivity and enhance the user experience.
    Clean and Modern Layout: Designed with modern web standards and best practices in mind.
    Technologies Used
    HTML: Structure and content of the website.
    CSS: Styling and layout of the website.
    JavaScript: Functionality and interactivity.

---
+++ custom.js
@@ -0,0 +1,8 @@
+// $(".card").hover(function(){
+//     $(this).find("label").css("background-color", "rgb(192, 74, 74)");
+//     $(this).find("button").css("background-color", "rgb(192, 74, 74)");
+// },function(){
+//     $(this).find("label").css("background-color", "rgb(64, 64, 64)");
+//     $(this).find("button").css("background-color", "rgb(64, 64, 64)");
+// }
+// );
